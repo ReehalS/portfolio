93641f5b56f65ce3a84c8be5a4a611149a68c1c1
SandeepR <sunny.reehal@gmail.com>
2024-05-21 11:52:33 -0700

    extra UI fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ function Footer() {
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                <AiFillGithub />
+                <AiFillGithub className="footer-icons"/>
               </a>
         </Col>
       </Row>
