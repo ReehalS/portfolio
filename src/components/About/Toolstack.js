d11abfb34b99a82c770da4a9bc45bf42dd230881
reehals <sunny.reehal@gmail.com>
2025-03-29 00:56:59 -0700

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import {
-  SiVisualstudiocode,
   SiPostman,
   SiSlack,
   SiFigma,
   SiTermius, 
   SiGithub
 } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
 
 function Toolstack() {
   return (
     <Container style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
       <div className="tech-icons">
-        <SiVisualstudiocode />
+        <VscVscode />
         <p>VS Code</p>
       </div>
       <div className="tech-icons">
